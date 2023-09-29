d103dc79c82e7f6ca99089f79bb802cbfee7f71e
acofalka <agnieszkac.2204@gmail.com>
2023-09-29 20:23:49 +0200

    Added webhooks endpoints

--- src/main/java/com/example/webhooks/controller/WebhookController.java
+++ src/main/java/com/example/webhooks/controller/WebhookController.java
@@ -1,14 +1,39 @@
 package com.example.webhooks.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/")
+@Slf4j
 public class WebhookController {
+    private String TOKEN = "TOKEN";
+    private String SUBSCRIBE_MODE = "subscribe";
+
     @GetMapping
     public String hello() {
         return "Hello :)";
+    }
+
+    @GetMapping("/webhooks")
+    public ResponseEntity<Integer> verify(@RequestHeader("hub.mode") String mode, @RequestHeader("hub.challenge") int challenge,
+                                 @RequestHeader("hub.verify_token") String verifyToken) {
+        if (SUBSCRIBE_MODE.equals(mode) && TOKEN.equals(verifyToken)) {
+            return new ResponseEntity<>(challenge, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @PostMapping("/webhooks")
+    public ResponseEntity<Void> processNotification(@RequestBody String body, @RequestHeader Map<String, String> headers) {
+        log.info("Body: " + body);
+        headers.forEach((key, value) -> {
+            log.info(String.format("Header '%s' = %s", key, value));
+        });
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
